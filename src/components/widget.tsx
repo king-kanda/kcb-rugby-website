a1caeead3e52638734ae0da42e229813e57ee610
king-kanda <stevenokanda@gmail.com>
2025-12-08 16:55:08 +0300

    experimentation

--- src/components/widget.tsx
+++ src/components/widget.tsx
@@ -5,8 +5,8 @@ export default function ChatWidgetWrapper() {
   useEffect(() => {
     const chatWidget = new ChatWidget({
       title: 'Squad Leader Nexus AI Prod',
-      appToken: "nw_R7-Vll42R0ORauQ0vIu0iVem",
-      // appToken: "nw_JhdTsyYDZEYpulY-Hn-zaiW4", 
+      // appToken: "nw_R7-Vll42R0ORauQ0vIu0iVem", // Prod Token
+      appToken: "nw_JhdTsyYDZEYpulY-Hn-zaiW4", // Demo Token
       sessionId: "user-12345", 
       // apiBaseUrl : "https://demo.nexuswave.ai",
       botAvatar: "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExbHVwbDg4MzAwamgya3Nic3BrcGdhaXZqNnNrbWcwODB6MGp0ajNhOCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/r3wko2AaqERGfkqvrw/giphy.gif"
