da522238c824446db8fddcd0e227d33c089776b9
king-kanda <stevenokanda@gmail.com>
2025-12-11 03:19:50 +0300

    panik

--- src/components/widget.tsx
+++ src/components/widget.tsx
@@ -5,10 +5,10 @@ export default function ChatWidgetWrapper() {
   useEffect(() => {
     const chatWidget = new ChatWidget({
       title: 'Squad Leader Nexus AI ',
-      appToken: "nw_cPfDQfEzLwL3rmX-ZIwKl8NK", // Dev Token
+      appToken: "nw_U8qkALWm5TZtLENhtKW6m1Tc", // Dev Token
       sessionId: "user-12345", 
       // apiBaseUrl : "https://demo.nexuswave.ai",
-      apiBaseUrl : "http://localhost:8082",
+      // apiBaseUrl : "http://localhost:8082",
       botAvatar: "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExbHVwbDg4MzAwamgya3Nic3BrcGdhaXZqNnNrbWcwODB6MGp0ajNhOCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/r3wko2AaqERGfkqvrw/giphy.gif"
     });
 
